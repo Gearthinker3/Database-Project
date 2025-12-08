c01ad84d65f8d27efbe04b4962dac7d6deb50a96
Gearthinker3 <Loganmatthews@gmail.com>
2025-12-07 18:32:24 -0700

    Added working query and major dropdown menu

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, request, render_template, session, redirect, url_for
 from db import test_connection, get_connection
 import psycopg2
 
-
-
 app = Flask(__name__)
 app.secret_key = "e3b6f7c2fd1e497bb3c1f0c39d3be24f1fa91d3f260a9fc715f68699fbd55e80"
+
 
 @app.route('/')
 def home():
     return render_template("login.html")
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -17,15 +17,12 @@ def login():
     password = request.form['password']
 
     try:
-        # test database connection
         conn = test_connection(user, password)
         conn.close()
 
-        # save credentials in session
         session['db_user'] = user
         session['db_pass'] = password
 
-        # redirect to search page
         return redirect(url_for('search'))
 
     except Exception as e:
@@ -39,33 +36,41 @@ def search():
         return redirect(url_for('home'))
 
     if request.method == 'GET':
-        # render search form
         return render_template("query.html")
 
-    # POST: process search query
     major = request.form.get('major')
-    state = request.form.get('state')
+    state = request.form.get('state').upper().strip()
 
     try:
         conn = get_connection(session['db_user'], session['db_pass'])
         cur = conn.cursor()
+
+        cur.execute("SET search_path TO jett_morrandez;")
+
         query = """
-            SELECT *
-            FROM institutions i
-            WHERE i.major = %s
-            AND i.state = %s;
+            SELECT s.Major, s.startingmediansalary, i.INSTNM
+            FROM programs p
+            JOIN degree_to_salary s ON p.majorid = s.majorid
+            JOIN institutions i ON p.UNITID = i.UNITID
+            WHERE i.STABBR = %s
+              AND s.Major = %s
+              AND p.CREDDESC = 'Bachelor''s Degree'
+            ORDER BY s.startingmediansalary DESC;
         """
-        values = (major, state)
-        cur.execute(query, values)
+
+        cur.execute(query, (state, major))
         results = cur.fetchall()
+
         cur.close()
         conn.close()
 
-        # render results page
         return render_template("results.html", results=results)
 
     except Exception as e:
         return f"Error: {e}"
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
